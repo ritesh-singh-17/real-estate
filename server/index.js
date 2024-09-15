d5e25c1831e5033fffc66627de284f9c5d14d364
riteshsingh633 <singh.riteshkumar0633@gmail.com>
2024-09-15 13:31:37 +0530

    updated server config

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-import path from 'path';
+import cors from 'cors'
 dotenv.config();
 
 mongoose
@@ -17,35 +17,25 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
-
 const app = express();
 
 app.use(express.json());
-
+app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
+const corsOptions = {
+  origin:'https://real-estate-r.vercel.app/',
+  credentials:true
+}
+
+app.use(cors(corsOptions));
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
-
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  return res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
